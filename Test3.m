f9b96037dcac4111c7d04503bddd3e5773b465d5
lustea0201 <alexandre.luster@epfl.ch>
2020-02-27 15:49:39 -0500

    Improved prediction, added another painting and adapt for different sizes of images

--- Test3.m
+++ Test3.m
@@ -1,80 +1,74 @@
 clear all 
 close all 
 
+painter = "monet"; % can chose "monet" or "vg"  (Van Gogh)
+
 photo1 = imread("src/photo1.jpg");
-painting1 = imread("src/monet1.jpg");
+painting1 = imread(strcat("src/", painter, "1.jpg"));
 photo2 = imread("src/photo2.jpg");
-painting2 = imread("src/monet2.jpg");
+painting2 = imread(strcat("src/", painter, "2.jpg"));
 photo3 = imread("src/photo3.jpg");
-painting3 = imread("src/monet3.jpg");
+painting3 = imread(strcat("src/", painter, "3.jpg"));
 photo4 = imread("src/photo4.jpg");
-painting4 = imread("src/monet4.jpg");
+painting4 = imread(strcat("src/", painter, "4.jpg"));
+
+to_predict = painting2; % This is the painting we want to create a photo from
+expected = photo2; % Expected result
+
+painting5 = imread("src/MONET.jpg");
 
 FOURIER_SIZE = [2048 2048];
 
-% figure(), imshow(I), title('I');
-% figure(), imshow(Image1), title('Image1'); 
-% figure(), imshow(painting1), title('painting1'); 
-% figure(), imshow(Image2), title('Image2'); 
-% figure(), imshow(painting2),title('painting2'); 
 
 % Get the FFT of the images (not centered)
-f1 = fft3(photo1, FOURIER_SIZE);
-f1_Monet = fft3(painting1, FOURIER_SIZE);
-f2_Monet = fft3(painting2, FOURIER_SIZE);
-% figure(), imshow(fftshift(f1)), title('Fourier1'); 
-% figure(), imshow(fftshift(f1_Monet)), title('Monet 1 '); 
-% figure(), imshow(fftshift(f2_Monet)),title('Monet 2'); 
+fphoto1 = fft3(photo1, FOURIER_SIZE);
+fphoto2 = fft3(photo2, FOURIER_SIZE);
+fphoto3 = fft3(photo3, FOURIER_SIZE);
+fphoto4 = fft3(photo4, FOURIER_SIZE);
+fpainting1 = fft3(painting1, FOURIER_SIZE);
+fpainting2 = fft3(painting2, FOURIER_SIZE);
+fpainting3 = fft3(painting3, FOURIER_SIZE);
+fpainting4 = fft3(painting4, FOURIER_SIZE);
+fpredict = fft3(to_predict, FOURIER_SIZE);
+fpainting5 = fft3(painting5, FOURIER_SIZE);
 
 
 % Obtain the filters
-filter1 = getKernel(f1, f1_Monet, FOURIER_SIZE);
-filter2 = getKernel(f2, f2_Monet, FOURIER_SIZE);
-filter3 = getKernel(f3, f3_Monet, FOURIER_SIZE);
-filteravg = (filter1 + filter2 + filter3)./3
+filter1 = getKernel(fphoto1, fpainting1, FOURIER_SIZE); 
+filter3 = getKernel(fphoto3, fpainting3, FOURIER_SIZE);
+filter4 = getKernel(fphoto4, fpainting4, FOURIER_SIZE);
+% We want to take the average of those 4 filters to get a filter more
+% representative of Monet's painting style
+filteravg = (filter1+ filter3 + filter4)./3; 
 
 
 % Predict Fourier representation of image 4
-outf1 = predictFourier(filter1, f4_Monet); 
-outf2 = predictFourier(filter2, f4_Monet); 
-outf3 = predictFourier(filter3, f4_Monet); 
-outavg = predictFourier(filteravg, f4_Monet); 
+fout = predictFourier(filteravg, fpredict); 
+fout5 = predictFourier(filteravg, fpainting5); 
 
 % Get the spatial domain representation 
-out1 = getSpatialRep(outf1, size(painting4));
-out2 = getSpatialRep(outf2, size(painting4));
-out3 = getSpatialRep(outf3, size(painting4));
-outavg = getSpatialRep(outavg, size(painting4));
+out = getSpatialRep(fout, size(to_predict));
+out5 = getSpatialRep(fout5, size(painting5));
 
-% display prediction
-figure()
-imshow(out1/255)
-title('out1')
-figure()
-imshow(out2/255)
-title('out2')
-figure()
-imshow(out3/255)
-title('out3')
-figure()
-imshow(outavg/255)
-title('out3')
+% display predictions
+out = getMedianFiltered(out, [6, 6]); 
+figure(1)
+imshow(out/255)
+title('What Monet saw')
+imwrite(out/255, "out/monet_photo2.jpg")
 
 
+out5 = getMedianFiltered(out5, [6, 6]); 
+figure(2)
+imshow(out5/255)
+title('What Monet saw')
+imwrite(out5/255, "out/monet_photo5.jpg")
 
 
-% save it
-% imwrite(out/255, "out/out.jpg")
-
-% display median-filtered version
-% med = getMedianFiltered(out, [5, 5]); 
-% figure()
-% imshow(med/255)
-% title('With median filtering')
 
 %display what we wanted to see
-figure()
-imshow(photo2)
+figure(3)
+imshow(expected)
 title('expected')
 
 function F = fft3(I, FOURIER_SIZE)
@@ -110,12 +104,12 @@ function If = getSpatialRep(F, s)
 % F: RGB image in the Fourier domain
 % Returns I, the corresponding image in the spatial domain
 
-    height = s(1); width = s(2)
+    height = s(1); width = s(2);
     I = zeros(size(F));
     I(:,:,1) = ifft2(F(:,:,1));
     I(:,:,2) = ifft2(F(:,:,2));
     I(:,:,3) = ifft2(F(:,:,3));
-    If = I(1:height, 1:width, : )
+    If = I(1:height, 1:width, : );
 end
 
 function MFI = getMedianFiltered(I, neighborhood)
