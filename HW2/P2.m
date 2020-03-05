f534ea61e6db82eef2ae0abe87950fef1103ff72
lustea0201 <alexandre.luster@epfl.ch>
2020-03-05 16:24:40 -0500

    Generates the CSV files with positions

--- HW2/P2.m
+++ HW2/P2.m
@@ -2,51 +2,63 @@ clear all
 close all 
 
 % Creating an empty 1024x1024 image
-SIZE = [1024, 1024];
-N_particles = 32; 
-max_speed = 32; 
-N_images = 10;
-SNratio = 100;
-
-[MAP, current_positions] = initializeMap(SIZE, N_particles);
-I = map2im(MAP, 20); 
-
-out_directory = strcat('out/', num2str(N_particles), 'particles/', num2str(max_speed), 'pixels_frame/', num2str(SNratio), '_1SN/');
-images_directory = strcat(out_directory,'/images')
-if ~exist(out_directory, 'dir')
-    mkdir(out_directory)
+SIZE = [102,102];
+N_images = 100;
+if exist('out', 'dir')
+    rmdir out s % remove all previous files
 end
+N_particles = [32, 128, 256, 512];
+max_speed = [4, 8, 32];
+SNratio = [20, 5, 2];
+total = size(N_particles, 2)*size(max_speed, 2)*size(SNratio, 2)*N_images;
+% Number of images to generate & save
 
-if ~exist(images_directory, 'dir')
-    mkdir(images_directory)
+for n = 1:size(N_particles, 2)
+    for s = 1:size(max_speed, 2)
+        for r = 1:size(SNratio, 2)
+            [MAP, current_positions] = initializeMap(SIZE, N_particles(n));
+            I = map2im(MAP, 20); 
+
+            out_directory = strcat('out/', num2str(N_particles(n)), 'particles/', num2str(max_speed(s)), 'pixels_frame/', num2str(SNratio(r)), '_1SN/');
+            images_directory = strcat(out_directory,'images/');
+            positions_directory = strcat(out_directory,'positions/');
+            mkdir(out_directory)
+            mkdir(images_directory)
+            mkdir(positions_directory)
+            
+
+            writerObj = VideoWriter(strcat(out_directory, 'Video.avi'));
+            writerObj.FrameRate = 10;
+            open(writerObj);
+            imwrite(I, strcat(images_directory, 'Image1.jpg'));
+            [I, map ] = imread(strcat(images_directory, 'Image1.jpg'));
+
+            frame = im2frame(I, gray(256));
+            writeVideo(writerObj, frame);
+            csvwrite(strcat(positions_directory, 'positions1.csv'), current_positions')
+
+
+            for f = 2:N_images
+                current = (n-1)*size(max_speed, 2)*size(SNratio, 2)*N_images + (s-1)*size(SNratio, 2)*N_images + (r-1)*N_images + f;
+                percentage_done = (100*current/total);
+                if (mod(percentage_done, 1) == 0)
+                    disp(percentage_done+ "% done")
+                end
+                [MAP, current_positions] = move(SIZE, current_positions, max_speed(s)); 
+                I = map2im(MAP, 20); 
+                filename = strcat(images_directory, 'Image', num2str(f), '.jpg');
+                imwrite(I, filename)
+                [I, map ] = imread(filename);
+                frame = im2frame(I, gray(256));
+                writeVideo(writerObj, frame);
+                csvwrite(strcat(positions_directory, 'positions', num2str(f),'.csv'), current_positions')
+            end
+
+
+            close(writerObj);
+        end
+    end
 end
-
-
-
-writerObj = VideoWriter(strcat(out_directory, '/Video.avi'));
-writerObj.FrameRate = 10;
-open(writerObj);
-
-imwrite(I, strcat(images_directory, '/Image1.jpg'));
-[I, map ] = imread(strcat(images_directory, '/Image1.jpg'));
-
-frame = im2frame(I, gray(256))
-writeVideo(writerObj, frame);
-
-for frame = 2:N_images
-    disp((100*frame/N_images)+ "% done")
-    [MAP, current_positions] = move(SIZE, current_positions, max_speed); 
-    I = map2im(MAP, 20); 
-    filename = strcat(images_directory, '/Image', num2str(frame), '.jpg');
-    imwrite(I, filename)
-    [I, map ] = imread(filename);
-    frame = im2frame(I, gray(256));
-    writeVideo(writerObj, frame);
-end
-
-
-close(writerObj);
-
 
 
 function [M, positions] = initializeMap(SIZE, N)
@@ -122,8 +134,8 @@ new = positions;
 for i = 1:size(positions,2)
     r = randi([0, speed], 1, 1);
     theta = 2*pi*randn(1,1);
-    new(1,i) = max(min(new(1,i) + floor(r*cos(theta)), 1024), 1);
-    new(2,i) = max(min(new(2,i) + floor(r*sin(theta)), 1024), 1);
+    new(1,i) = max(min(new(1,i) + floor(r*cos(theta)), SIZE(1)), 1);
+    new(2,i) = max(min(new(2,i) + floor(r*sin(theta)), SIZE(2)), 1);
 end
 M2 = 255*ones(SIZE);
 for i = 1:size(new, 2)
